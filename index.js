05b5120c6398abbb5f9b6c0ef40ed670be089b2c
Evelyn Goris <goris.evelyn@gmail.com>
2020-09-23 14:27:44 -0500

    added utils folder with generateMarkdown.js inside; added index.js, pakage.json, gitignore, README.md, README-1.md files to the root

---
+++ index.js
@@ -0,0 +1,68 @@
+// array of questions for user
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide your project's description"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please provide the installation instructions"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please provide the usage information"
+    },
+    {
+        type: "input",
+        name: "licence",
+        message: "Please provide the app licence or your badge link"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Please provide the contribution guidelines"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Please provide tests instructions"
+    },
+    
+    
+    {
+        type: "input",
+        name: "username",
+        message: "What is your github user name?"
+    },
+    {
+        type: "input",
+        name: "repo",
+        message: "What is your repo link?"
+    },
+    {
+        type: "input",
+        name: "badge",
+        message: "Please provide the badges links that you want"
+    },
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
